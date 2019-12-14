360b1045696bf000a49de5f6536ce7ae029dab40
k-dang <dangkevin12@gmail.com>
2019-12-14 18:08:30 -0500

    allow credentials from environment

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ import datetime
 from datetime import timedelta
 import os
 from dotenv import load_dotenv
+import json
 
 # firebase
 import firebase_admin
@@ -20,7 +21,8 @@ from app.Models.DataSetReader import DataSetReader
 load_dotenv()
 
 # setup firebase admin sdk
-cred = credentials.Certificate(os.getenv("firebase_credentials_path"))
+cred_dict = json.loads(os.getenv("firebase_credentials_json"))
+cred = credentials.Certificate(cred_dict)
 firebase_admin.initialize_app(cred, {
     'storageBucket': 'twitter-api-store.appspot.com'
 })
@@ -43,51 +45,54 @@ def check_cache():
     if (local_blob_size != storage_blob_size):
         blob.download_to_filename(destination_file_name)
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
 
-# body content must contain date
-# {
-#   "date": "2019-11-21"
-# }
-# return single day sentiment
-@app.route('/api/tesla/sentiment/day', methods=['POST'])
-def get_sentiment():
-    data = request.json
+    # body content must contain date
+    # {
+    #   "date": "2019-11-21"
+    # }
+    # return single day sentiment
+    @app.route('/api/tesla/sentiment/day', methods=['POST'])
+    def get_sentiment():
+        data = request.json
 
-    tesla_twitter_set = DataSetReader(destination_file_name)
-    tesla_twitter_set.drop_dups_add_sentiment()
+        tesla_twitter_set = DataSetReader(destination_file_name)
+        tesla_twitter_set.drop_dups_add_sentiment()
 
-    date = datetime.datetime.strptime(data['date'], '%Y-%m-%d').date()
-    date_before = date + timedelta(days=1)
+        date = datetime.datetime.strptime(data['date'], '%Y-%m-%d').date()
+        date_before = date + timedelta(days=1)
 
-    after = data['date']
-    before = date_before.strftime('%Y-%m-%d')
-    avg_sentiment_day = tesla_twitter_set.get_sentiment_range(after, before)
+        after = data['date']
+        before = date_before.strftime('%Y-%m-%d')
+        avg_sentiment_day = tesla_twitter_set.get_sentiment_range(after, before)
 
-    return jsonify({
-        'sentiment': avg_sentiment_day
-    })
+        return jsonify({
+            'sentiment': avg_sentiment_day
+        })
 
-# body content must contain the range as
-# {
-#   "from": "2019-11-21"
-#   "to": "2019-11-29"
-# }
-# return list of sentiments, 1 for each day
-@app.route('/api/tesla/sentiment/range', methods=['POST'])
-def get_sentiment_range():
-    data = request.json
+    # body content must contain the range as
+    # {
+    #   "from": "2019-11-21"
+    #   "to": "2019-11-29"
+    # }
+    # return list of sentiments, 1 for each day
+    @app.route('/api/tesla/sentiment/range', methods=['POST'])
+    def get_sentiment_range():
+        data = request.json
 
-    # check_cache()
-    tesla_twitter_set = DataSetReader(destination_file_name)
-    tesla_twitter_set.drop_dups_add_sentiment()
+        # check_cache()
+        tesla_twitter_set = DataSetReader(destination_file_name)
+        tesla_twitter_set.drop_dups_add_sentiment()
 
-    filtered_df = tesla_twitter_set.get_filtered_df(data['from'], data['to'] + '23:59:59')
-    result_df = filtered_df.groupby(['created_at_date']).mean()
-    sentiment_df = result_df['sentiment']
-    sentiment_df.index = sentiment_df.index.map(str)
+        filtered_df = tesla_twitter_set.get_filtered_df(data['from'], data['to'] + '23:59:59')
+        result_df = filtered_df.groupby(['created_at_date']).mean()
+        sentiment_df = result_df['sentiment']
+        sentiment_df.index = sentiment_df.index.map(str)
 
-    result = sentiment_df.to_dict()
-    return jsonify({
-        'sentiments': result
-    })
+        result = sentiment_df.to_dict()
+        return jsonify({
+            'sentiments': result
+        })
+
+    return app
